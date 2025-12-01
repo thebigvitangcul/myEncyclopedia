be9f675a79f5a41d4084c302cf985f45020e957f
Dylan Vitangcul <202301168@iacademy.edu.ph>
2025-12-01 20:24:01 +0800

    pdf loaded, scrollable, viewable, good stuff

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,11 +12,13 @@ class _HomePageState extends State<HomePage> {
 
   late PdfControllerPinch pdfControllerPinch;
 
+  int totalPageCount = 0, currentPage = 1;
+
   @override
   void initState() {
     super.initState();
-    pdfControllerPinch = new PdfControllerPinch(
-      document: PdfDocument.openAsset("assets\pdfs\myEncyclopedia_Documentation.pdf"));
+    pdfControllerPinch = PdfControllerPinch(
+      document: PdfDocument.openAsset("assets/pdfs/flutter_tutorial.pdf"));
   }
 
   @override
@@ -31,6 +33,55 @@ class _HomePageState extends State<HomePage> {
         ),
         backgroundColor: Colors.red,
       ),
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    return Column(
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [Text("Total Pages: ${totalPageCount}"),
+          IconButton(
+            onPressed: () {
+              pdfControllerPinch.previousPage(duration: Duration(milliseconds: 500,), curve: Curves.linear,);
+            },
+             icon: Icon(
+              Icons.arrow_back,
+              ),
+            ),
+          Text("Current Page: ${currentPage}"),
+          IconButton(
+            onPressed: () {
+              pdfControllerPinch.nextPage(duration: Duration(milliseconds: 500,), curve: Curves.linear,);
+            },
+             icon: Icon(
+              Icons.arrow_forward,
+              ),
+            ), 
+          ],
+        ),
+        _pdfView(),
+      ],
+    );
+  }
+
+  Widget _pdfView () {
+    return Expanded(child: 
+    PdfViewPinch(controller: pdfControllerPinch, onDocumentLoaded: (doc) {
+      setState(() {
+        totalPageCount = doc.pagesCount;
+      });
+    },
+    onPageChanged: (page) {
+      setState(() {
+        currentPage = page;
+      });
+    },
+    ),
     );
   }
 }
